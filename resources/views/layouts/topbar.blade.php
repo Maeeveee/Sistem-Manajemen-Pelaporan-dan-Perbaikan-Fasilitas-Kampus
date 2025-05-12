d78550fcef6053b0830d4630642476f4e22c993f
Naditya206 <aditandino2019211@gmail.com>
2025-05-12 20:36:11 +0700

    Update nama pada topbar sesuai login

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -145,12 +145,12 @@
           <a class="nav-link dropdown-toggle pt-1 px-0" href="#" role="button" data-bs-toggle="dropdown"
             aria-expanded="false">
             <div class="media d-flex align-items-center">
-              <img class="avatar rounded-circle" alt="Image placeholder" src="/assets/img/team/profile-picture-1.jpg">
-              <div class="media-body ms-2 text-dark align-items-center d-none d-lg-block">
-                <span
-                  class="mb-0 font-small fw-bold text-gray-900">{{  auth()->user()->first_name ? auth()->user()->first_name . ' ' . auth()->user()->last_name : 'User Name'}}</span>
-              </div>
-            </div>
+    <img class="avatar rounded-circle" alt="Image placeholder" src="/assets/img/team/profile-picture-1.jpg">
+    <div class="media-body ms-2 text-dark align-items-center d-none d-lg-block">
+        <span class="mb-0 font-small fw-bold text-gray-900">{{ auth()->user()->name ?? 'User Name' }}</span>
+    </div>
+</div>
+
           </a>
           <div class="dropdown-menu dashboard-dropdown dropdown-menu-end mt-2 py-1">
             <a class="dropdown-item d-flex align-items-center" href="/profile">
