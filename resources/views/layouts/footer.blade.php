5dbb8cc74382642cc233b929a402c8010ce295c4
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2025-05-14 09:26:30 +0700

    update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,8 @@
 <footer class="bg-white rounded shadow p-5 mb-4 mt-4">
     <div class="row">
-        <div class="col-12 col-md-4 col-xl-6 mb-4 mb-md-0">
-            <p class="mb-0 text-center text-lg-start">© 2019-<span class="current-year"></span> <a
-                    class="text-primary fw-normal" href="https://themesberg.com" target="_blank">Themesberg &
-                </a><a href="https://updivision.com/" target="_blank">Updivision</a></p>
+        <div class="col-12 d-flex align-items-center justify-content-center">
+            <p class="mb-0 text-center">@<span class="current-year"></span> <a
+                        class="text-primary fw-normal" href="http://127.0.0.1:8000/login" target="_blank">Sistem Manajemen Pelaporan dan Perbaikan Fasilitas Kampus</p>
         </div>
     </div>
 </footer>
