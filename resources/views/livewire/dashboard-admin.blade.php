e9dce4f26c90f2c2da684343b95d2cec661804e7
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2025-05-25 11:52:56 +0700

    fixing minor problem on admin dashboard

--- resources/views/livewire/dashboard-admin.blade.php
+++ resources/views/livewire/dashboard-admin.blade.php
@@ -1,28 +1,16 @@
-<div>
-    {{-- In work, do what you enjoy. --}}
-</div>
+@section('breadcrumbs')
+    @php
+        $breadcrumbs = [
+            'Admin' => '',
+            'laporan Perbaikan' => route('dashboard-admin'),
+        ];
+    @endphp
+    @include('layouts.breadcrumb', ['breadcrumbs' => $breadcrumbs])
+@endsection
 <title>Dashboard -Teknisi</title>
-
 <body>
-
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
     <div class="d-block mb-4 mb-md-0">
-        <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
-            <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
-                <li class="breadcrumb-item">
-                    <a href="#">
-                        <svg class="icon icon-xxs" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
-                            </path>
-                        </svg>
-                    </a>
-                </li>
-                <li class="breadcrumb-item"><a href="#">Teknisi</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
-            </ol>
-        </nav>
         <h2 class="h4">Laporan Perbaikan Fasilitas</h2>
     </div>
 </div>
@@ -43,9 +31,9 @@
             </div>
             <select class="form-select fmxw-200 d-none d-md-inline" id="statusFilter" aria-label="Filter status">
                 <option value="all" selected>Semua</option>
-                <option value="Verifikasi">Verifikasi</option>
-                <option value="Pending">Pending</option>
-                <option value="Reject">Reject</option>
+                <option value="verifikasi">Verifikasi</option>
+                <option value="pending">Pending</option>
+                <option value="reject">Reject</option>
             </select>
 
 
