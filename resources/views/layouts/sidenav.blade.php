22c9fa22d637bcdc0f3ac8d4c508544bcd71e339
rafiirajendra <rafi53417@gmail.com>
2025-05-26 10:35:32 +0700

    membetulkan dropdown admin

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -168,7 +168,7 @@
       </li>
       
       <li class="nav-item">
-        <span class="nav-link collapsed d-flex justify-content-between align-items-center" data-bs-toggle="collapse" data-bs-target="#submenu-laravel" aria-expanded="true">
+        <span class="nav-link collapsed d-flex justify-content-between align-items-center" data-bs-toggle="collapse" data-bs-target="#submenu-admin" aria-expanded="true">
           <span>
             <span class="sidebar-icon"><i class="fas fa-users me-2" style="color: White;"></i></span>
             <span class="sidebar-text" style="color: White;">Admin</span>
