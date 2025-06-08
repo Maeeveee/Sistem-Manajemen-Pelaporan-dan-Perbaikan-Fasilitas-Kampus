21991e5740471abb9539623e7d59563474db8ffa
Naditya206 <aditandino2019211@gmail.com>
2025-06-08 16:18:00 +0700

    Update profile topbar

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -36,13 +36,10 @@
                     </a>
                     <div class="dropdown-menu dashboard-dropdown dropdown-menu-end mt-2 py-1">
                         <a class="dropdown-item d-flex align-items-center" href="/profile">
-                            <svg class="dropdown-icon text-gray-400 me-2" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                            My Profile
+                            <img class="avatar rounded-circle me-2" alt="Profile Photo"
+                                src="{{ auth()->user()->profile_photo_path ? Storage::url(auth()->user()->profile_photo_path) : asset('assets/img/team/default_photo_profile.jpg') }}"
+                                style="width: 28px; height: 28px; object-fit: cover;">
+                            Profil Akun
                         </a>
                         <div role="separator" class="dropdown-divider my-1"></div>
                         <a class="dropdown-item">
